ae52894a89d9575d3f5d4b28bea0da09fe5cdb0f
Noah Martino <treeclan12@gmail.com>
2018-08-05 16:14:47 -0400

    Built More Fuels Mod Light 1.0.0 for Minecraft 1.11.x

---
+++ build/sources/main/java/com/bored/morefuelsmodlight/fuels/MinecartWithChest.java
@@ -0,0 +1,17 @@
+//Vanilla Chest is 300 ticks.
+package com.bored.morefuelsmodlight.fuels;
+
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.IFuelHandler;
+
+public class MinecartWithChest implements IFuelHandler {
+
+	@Override
+	public int getBurnTime(ItemStack fuel){
+		if(fuel.getItem() == Items.CHEST_MINECART){
+			return 400;
+		}
+		return 0;
+	}
+}
---
+++ build/sources/main/java/com/bored/morefuelsmodlight/fuels/TNT.java
@@ -0,0 +1,17 @@
+package com.bored.morefuelsmodlight.fuels;
+
+import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.IFuelHandler;
+
+public class TNT implements IFuelHandler {
+
+	@Override
+	public int getBurnTime(ItemStack fuel){
+		if(fuel.getItem() == Item.getItemFromBlock(Blocks.TNT)){
+			return 8100;
+		}
+		return 0;
+	}
+}
